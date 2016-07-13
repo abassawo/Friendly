1fa0b73dcffce6115bdc5d12c44bc17d6612716a
Abass Bayo-Awoyemi <Abass@C4Qs-MacBook-Pro.local>
2016-07-13 00:06:20 -0400

    init commit

---
+++ app/src/main/java/momentum/c4q/abass/friendly/WidgetService.java
@@ -0,0 +1,18 @@
+package momentum.c4q.abass.friendly;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Abass on 7/12/16.
+ */
+public class WidgetService extends Service {
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
